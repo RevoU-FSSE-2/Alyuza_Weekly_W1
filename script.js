e4a89f4c88d5a134136db96204fcc74a5ab3870a
alyuza <alyuza.sat@gmail.com>
2023-06-15 17:40:42 +0700

    add comments

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function button(){
-    var letters = /^[A-Za-z]+$/;
+function button(){ //on click button
     let x = 'Hi ';
     let y = document.getElementById('name').value;
     let z = ', Nice to Meet You ^^';
     let message = x + y + z;
-
-    if(y.match(letters)){
+    var letters = /^[A-Za-z]+$/; //Letters Only
+    
+    if(y.match(letters)){ 
         alert(message)
     }else{
         alert('Please Input Correct Name')
